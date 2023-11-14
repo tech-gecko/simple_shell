550fd72f3010843d0824e48b0f3ef496a4175220
akinremimoses <akinremimoses93@gmail.com>
2023-11-14 00:26:56 -0800

    akmosesfunctions

--- main.h
+++ main.h
@@ -18,5 +18,8 @@ char **tokenizer(char *lineptr, char *delim);
 char **wrapper_func(void);
 ssize_t _getline(char **lineptr, size_t *n, int fd);
 ssize_t _read(char *buffer, int fd, size_t *n);
+size_t strlen(const char *str)
+int strcmp(const char *str1, const char *str2)
+
 
 #endif
